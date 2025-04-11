d6d25f65c4d7de8f980b894117af20a082d8519a
dippal513 <dip.pal.513@gmail.com>
2025-04-12 01:35:24 +0600

    feat(delete-review): add user review cleanup after successful deletion

--- src/app/api/delete-review/route.ts
+++ src/app/api/delete-review/route.ts
@@ -1,5 +1,6 @@
 import { connectDB } from "@/src/db/connection";
 import Review from "@/src/models/reviews.model";
+import User from "@/src/models/user.model";
 import { NextResponse } from "next/server";
 
 export async function DELETE(request: Request) {
@@ -15,8 +16,14 @@ export async function DELETE(request: Request) {
         { status: 400 }
       );
     }
-    
     const deleted = await Review.findByIdAndDelete(id); // Assume the review was deleted successfully
+    if (deleted) {
+      const user = await User.findById(deleted.userId); // Assuming `userId` is a field in the review model
+      if (user) {
+      user.reviews = user.reviews.filter((reviewId: string) => reviewId !== id);
+      await user.save();
+      }
+    }
 
     if (deleted) {
       return NextResponse.json(
