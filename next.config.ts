6a85e85b6f89108b42ab283b0a87b8bf9dd41c99
dippal513 <dip.pal.513@gmail.com>
2025-08-22 02:28:49 +0600

    source file fixed

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,14 @@
-
-import withPWA from 'next-pwa';
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
 
 // The main Next.js configuration object.
 const nextConfig = {
-  // Add other Next.js configurations here as needed.
   reactStrictMode: true,
   swcMinify: true,
+  // Add other Next.js configurations here as needed.
 };
 
-// Wrap the nextConfig with the withPWA plugin.
 // This exports the combined configuration.
-export default withPWA({
-  ...nextConfig,
-  pwa: {
-    // The destination directory for the service worker file.
-    dest: 'public',
-    // In development, the PWA plugin is disabled by default.
-    // Set this to true to enable it during development for testing.
-    disable: process.env.NODE_ENV === 'development',
-  },
-});
+module.exports = withPWA(nextConfig);
